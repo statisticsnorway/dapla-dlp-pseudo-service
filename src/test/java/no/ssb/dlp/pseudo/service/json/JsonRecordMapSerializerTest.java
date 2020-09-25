33f6567e4e9807d132a98c527f39cfc0d21a59e5
Øyvind Strømmen <insula.ventus@gmail.com>
2020-09-25 13:01:51 +0200

    Compare json using JSONAssert in unit test
    Comparing by string would fail occasionally due to non deterministic
    field ordering.

--- src/test/java/no/ssb/dlp/pseudo/service/json/JsonRecordMapSerializerTest.java
+++ src/test/java/no/ssb/dlp/pseudo/service/json/JsonRecordMapSerializerTest.java
@@ -3,7 +3,10 @@ package no.ssb.dlp.pseudo.service.json;
 import com.google.common.collect.Lists;
 import io.reactivex.Flowable;
 import org.apache.commons.lang3.StringUtils;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.util.Map;
 
@@ -12,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JsonRecordMapSerializerTest {
 
     @Test
-    void thatSerializeFlowableWorks() {
+    void thatSerializeFlowableWorks() throws JSONException {
         Flowable<Map<String, Object>> records = Flowable.fromArray(
                 Map.of("person", Map.of("name", Map.of("firstName", "Donald", "lastName", "Duck"))),
                 Map.of("person", Map.of("name", Map.of("firstName", "Bolla", "lastName", "Bollerud")))
@@ -40,6 +43,6 @@ class JsonRecordMapSerializerTest {
                 """;
 
         String got = String.join("", Lists.newArrayList(new JsonRecordMapSerializer().serialize(records).blockingIterable()));
-        assertThat(got).isEqualToIgnoringNewLines(StringUtils.deleteWhitespace(want));
+        JSONAssert.assertEquals(want, got, JSONCompareMode.STRICT);
     }
 }
