5b5543101c88d29ca338fdf4501257a29ecdeb29
Kenneth Leine Schulstad <ks@udp.no>
2021-04-09 10:48:04 +0200

    Improve export service logging

--- src/main/java/no/ssb/dlp/pseudo/service/export/ExportService.java
+++ src/main/java/no/ssb/dlp/pseudo/service/export/ExportService.java
@@ -1,8 +1,11 @@
 package no.ssb.dlp.pseudo.service.export;
 
 import com.google.common.base.CharMatcher;
+import com.google.common.base.Stopwatch;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.MediaType;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
 import lombok.Builder;
@@ -28,6 +31,7 @@ import no.ssb.dlp.pseudo.service.datasetmeta.DatasetMetaService;
 import no.ssb.dlp.pseudo.service.pseudo.PseudoConfig;
 import no.ssb.dlp.pseudo.service.pseudo.PseudoSecrets;
 import no.ssb.dlp.pseudo.service.useraccess.UserAccessService;
+import no.ssb.dlp.pseudo.service.useraccess.UserAccessService.AccessCheckRequest;
 
 import javax.inject.Singleton;
 import javax.validation.constraints.NotNull;
@@ -90,53 +94,42 @@ public class ExportService {
     }
 
     public Single<DatasetExportResult> export(DatasetExport e) {
-        CatalogService.Dataset datasetInfo = catalogService.getDataset(GetDatasetRequest.builder()
-          .path(e.getSourceDatasetId().getPath())
-          .timestamp(e.getSourceDatasetId().getVersion())
-          .build()).blockingGet()
-          .getDataset();
+        // Retrieve dataset information from the catalog service
+        CatalogService.Dataset datasetInfo = getDatasetInfo(e.getSourceDatasetId()).orElseThrow(
+          () -> new ExportServiceException("Unable to resolve dataset '%s' in catalog".formatted(e.getSourceDatasetId().getPath()))
+        );
 
+        // Check if the user has access to export the dataset
         UserAccessService.DatasetPrivilege accessPrivilege = e.getDepseudonymize()
           ? UserAccessService.DatasetPrivilege.DEPSEUDO
           : UserAccessService.DatasetPrivilege.READ;
-
-        Boolean hasAccess = userAccessService.hasAccess(UserAccessService.AccessCheckRequest.builder()
-          .userId(e.getUserId())
-          .privilege(accessPrivilege)
-          .path(datasetInfo.getId().getPath())
-          .state(datasetInfo.getState())
-          .valuation(datasetInfo.getValuation())
-          .build()).blockingGet();
-
-        if (! hasAccess) {
-            throw new ExportServiceException("User %s does not have %s access to ".formatted(e.getUserId(), accessPrivilege, datasetInfo.getId().getPath()));
+        if (! hasAccess(e.getUserId(), accessPrivilege, datasetInfo)) {
+            throw new ExportServiceException("User %s does not have %s access to %s".formatted(e.getUserId(), accessPrivilege, datasetInfo.getId().getPath()));
         }
 
+        // Initialize depseudo mechanism if needed - else use a noOp interceptor
         FieldInterceptor fieldPseudoInterceptor = e.getDepseudonymize()
           ? initPseudoInterceptor(e.getPseudoConfig(), datasetInfo.datasetUri())
           : FieldInterceptor.noOp();
 
-        String targetName = Optional.ofNullable(e.getTargetName())
-          .orElse(e.getSourceDatasetId().getPath().replaceFirst(".*/([^/?]+).*", "$1"));
-        MediaType targetContentType = MoreMediaTypes.validContentType(e.getTargetContentType());
-
         // Initiate record stream
+        log.debug("Read parquet records");
         Flowable<Map<String, Object>> records = datasetClient.readParquetRecords(datasetInfo.datasetUri(), e.getColumnSelectors(), fieldPseudoInterceptor);
 
         // Serialize records
+        MediaType targetContentType = MoreMediaTypes.validContentType(e.getTargetContentType());
+        log.debug("Serialize records as %s".formatted(targetContentType));
         Flowable<String> serializedRecords = RecordMapSerializerFactory.newFromMediaType(targetContentType).serialize(records);
 
-        Zips.ZipOptions zipOptions = zipOpts()
-          .password(e.getCompression().getPassword())
-          .encryptionMethod(e.getCompression().getEncryption())
-          .build();
-
-        // Compress and encrypt stream contents
-        Flowable<byte[]> compressedRecords = Zips.zip(serializedRecords, filenameOf(targetName, e.getTargetContentType()), zipOptions);
-
-        String targetUri = PathJoiner.joinWithoutLeadingOrTrailingSlash(e.getTargetPath(), filenameOf(targetName, e.getCompression().getType()));
+        // Encrypt and compress stream contents
+        if (e.getTargetName() == null) {
+            e.setTargetName(e.getSourceDatasetId().getPath().replaceFirst(".*/([^/?]+).*", "$1"));
+        }
+        Flowable<byte[]> compressedRecords = encryptAndCompress(e, serializedRecords);
 
         // Upload stream contents
+        String targetUri = PathJoiner.joinWithoutLeadingOrTrailingSlash(e.getTargetPath(), filenameOf(e.getTargetName(), e.getCompression().getType()));
+        log.debug("Uploading results to %s".formatted(targetUri));
         return storageBackend
           .write(targetUri, compressedRecords)
           .timeout(30, TimeUnit.SECONDS)
@@ -149,6 +142,62 @@ public class ExportService {
           });
     }
 
+    /**
+     * Encrypt and compress serialized records to temporary file storage
+     */
+    Flowable<byte[]> encryptAndCompress(DatasetExport e, Flowable<String> serializedRecords) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        Zips.ZipOptions zipOptions = zipOpts()
+          .password(e.getCompression().getPassword())
+          .encryptionMethod(e.getCompression().getEncryption())
+          .build();
+        log.debug("Compress and encrypt serialized stream to temporary file. Encryption type: %s, Content name: %s. This can take some time...".formatted(e.getCompression().getEncryption(), e.getTargetName()));
+        Flowable<byte[]> compressedRecords = Zips.zip(serializedRecords, filenameOf(e.getTargetName(), e.getTargetContentType()), zipOptions);
+        log.debug("Compression/encryption done in %s" .formatted(stopwatch.elapsed()));
+        return compressedRecords;
+    }
+
+    /**
+     * Retrieve information about a dataset from the catalog service
+     *
+     * @param datasetId with path and closest dataset timestamp
+     * @return dataset info including bucket name, path, version, valuation, etc
+     */
+    Optional<CatalogService.Dataset> getDatasetInfo(DatasetId datasetId) {
+        GetDatasetRequest request = GetDatasetRequest.builder()
+          .path(datasetId.getPath())
+          .timestamp(datasetId.getVersion())
+          .build();
+        log.debug("getDatasetInfo %s".formatted(request));
+        CatalogService.Dataset datasetInfo = catalogService.getDataset(request)
+          .blockingGet()
+          .getDataset();
+        log.debug("datasetInfo %s".formatted(datasetInfo));
+        return Optional.ofNullable( datasetInfo);
+    }
+
+    /**
+     * Check if a given user has a specific privileged access to a given dataset
+     *
+     * @param userId e.g. firstname.lastname@ssb.no
+     * @param accessPrivilege CREATE, READ, UPDATE, DELETE, DEPSEUDO
+     * @param datasetInfo dataset info retrieved from the catalog service
+     * @return true iff the user has the conditional access
+     */
+    boolean hasAccess(String userId, UserAccessService.DatasetPrivilege accessPrivilege, CatalogService.Dataset datasetInfo) {
+        AccessCheckRequest request = AccessCheckRequest.builder()
+          .userId(userId)
+          .privilege(accessPrivilege)
+          .path(datasetInfo.getId().getPath())
+          .state(datasetInfo.getState())
+          .valuation(datasetInfo.getValuation())
+          .build();
+        log.debug("Check access %s".formatted(request));
+        boolean hasAccess = userAccessService.hasAccess(request).blockingGet();
+        log.debug("hasAccess: %s".formatted(hasAccess));
+        return hasAccess;
+    }
+
     String filenameOf(String contentName, MediaType contentType) {
         String timestamp = DateTimeFormatter.ofPattern("yyyyMMdd")
           .withZone(ZoneOffset.UTC)
@@ -159,12 +208,14 @@ public class ExportService {
     }
 
     FieldPseudoInterceptor initPseudoInterceptor(PseudoConfig pseudoConfig, DatasetUri datasetUri) {
+        log.debug("Initializing depseudonymization mechanism");
         if (pseudoConfig == null || pseudoConfig.getRules().isEmpty()) {
             log.info("No explicit pseudo config provided. Checking for pseudo config in .dataset-meta.json");
             pseudoConfig = datasetMetaService.readDatasetPseudoConfig(datasetUri);
         }
         List<PseudoFuncRule> pseudoRules = pseudoConfig.getRules();
 
+        // TODO: Fail if no pseudo rules could be resolved?
         log.info(pseudoRules.isEmpty()
           ? "No pseudo config provided. Target dataset will not be depseudonymized"
           : "Pseudo rules " + pseudoRules);
@@ -174,10 +225,6 @@ public class ExportService {
     }
 
     class ExportServiceException extends RuntimeException {
-        public ExportServiceException(String message, Throwable cause) {
-            super(message, cause);
-        }
-
         public ExportServiceException(String message) {
             super(message);
         }
