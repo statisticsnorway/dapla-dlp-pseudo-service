b8e9e85e342e4f296c90cafcab82067fa53c1516
Kenneth Leine Schulstad <ks@udp.no>
2021-12-21 09:58:32 +0100

    Support (de)pseudonymizing csv files

--- src/main/java/no/ssb/dlp/pseudo/service/pseudo/PseudonymizerFactory.java
+++ src/main/java/no/ssb/dlp/pseudo/service/pseudo/PseudonymizerFactory.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import no.ssb.dlp.pseudo.core.FieldPseudonymizer;
 import no.ssb.dlp.pseudo.core.PseudoFuncRule;
 import no.ssb.dlp.pseudo.core.StreamPseudonymizer;
+import no.ssb.dlp.pseudo.core.csv.CsvStreamPseudonymizer;
+import no.ssb.dlp.pseudo.core.file.MoreMediaTypes;
 import no.ssb.dlp.pseudo.core.json.JsonStreamPseudonymizer;
 import no.ssb.dlp.pseudo.core.map.RecordMapPseudonymizer;
 
@@ -34,11 +36,20 @@ public class PseudonymizerFactory {
         return new JsonStreamPseudonymizer(recordMapPseudonymizer);
     }
 
+    public CsvStreamPseudonymizer newCsvStreamPseudonymizer(Collection<PseudoFuncRule> rules) {
+        RecordMapPseudonymizer recordMapPseudonymizer = newRecordMapPseudonymizer(rules);
+        return new CsvStreamPseudonymizer(recordMapPseudonymizer);
+    }
+
     public StreamPseudonymizer newStreamPseudonymizer(Collection<PseudoFuncRule> rules, MediaType mediaType) {
         mediaType = Optional.ofNullable(mediaType).orElse(MediaType.APPLICATION_JSON_TYPE);
         if (MediaType.APPLICATION_JSON.equals(mediaType.toString())) {
             return newJsonStreamPseudonymizer(rules);
         }
+        else if (MoreMediaTypes.TEXT_CSV.equals(mediaType.toString())) {
+            return newCsvStreamPseudonymizer(rules);
+        }
+
         throw new IllegalArgumentException("No StreamPseudonymizer found for media type " + mediaType);
     }
 
