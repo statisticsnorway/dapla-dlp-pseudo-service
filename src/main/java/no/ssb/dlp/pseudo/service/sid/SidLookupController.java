25be024e02b2c75acc43a506097c548943932f0e
Kenneth Leine Schulstad <kls@rubberduck.no>
2023-01-05 13:21:12 +0100

    Add SID lookup controller (#21)

---
+++ src/main/java/no/ssb/dlp/pseudo/service/sid/SidLookupController.java
@@ -0,0 +1,77 @@
+package no.ssb.dlp.pseudo.service.sid;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Error;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.http.hateoas.Link;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.ssb.dlp.pseudo.service.security.PseudoServiceRole;
+
+@RequiredArgsConstructor
+@Controller("/sid")
+@Slf4j
+@Secured(SecurityRule.IS_AUTHENTICATED)
+@Tag(name = "SID operations")
+public class SidLookupController {
+    private final SidCache sidCache;
+
+    @Secured({PseudoServiceRole.ADMIN})
+    @ExecuteOn(TaskExecutors.IO)
+    @Get("/fnr/{fnr}")
+    public HttpResponse<SidInfo> lookupFnr(@PathVariable String fnr) {
+        SidItem sidItem = sidCache.getSidItemForFnr(fnr)
+                .orElseThrow(() -> new NoSidMappingFoundException("No SID matching fnr=" + fnr));
+
+        return HttpResponse.ok(SidInfo.from(sidItem));
+    }
+
+    @Secured({PseudoServiceRole.ADMIN})
+    @ExecuteOn(TaskExecutors.IO)
+    @Get("/snr/{snr}")
+    public HttpResponse<SidInfo> lookupSnr(@PathVariable String snr) {
+        SidItem sidItem = sidCache.getSidItemForSnr(snr)
+                .orElseThrow(() -> new NoSidMappingFoundException("No SID matching snr=" + snr));
+
+        return HttpResponse.ok(SidInfo.from(sidItem));
+    }
+
+    @Data
+    @Builder
+    public static class SidInfo {
+        private final String sid;
+        private final String currentFnr;
+
+        public static SidInfo from(SidItem sidItem) {
+            return SidInfo.builder()
+                    .sid(sidItem.getSnr())
+                    .currentFnr(sidItem.getFnrCurrent())
+                    .build();
+        }
+    }
+
+    @Error
+    public HttpResponse<JsonError> noSidMappingFoundError(HttpRequest request, NoSidMappingFoundException e) {
+        JsonError error = new JsonError(e.getMessage())
+                .link(Link.SELF, Link.of(request.getUri()));
+        return HttpResponse.<JsonError>notFound().body(error);
+    }
+
+    public static class NoSidMappingFoundException extends RuntimeException {
+        public NoSidMappingFoundException(String message) {
+            super(message);
+        }
+    }
+
+}
--- src/main/java/no/ssb/dlp/pseudo/service/sid/SidReader.java
+++ src/main/java/no/ssb/dlp/pseudo/service/sid/SidReader.java
@@ -24,6 +24,8 @@ public class SidReader {
     private static final int DATO_FNRNAA = 8;
     private static final int DATO_SNR = 8;
     private static final int RDATO_SNR = 8;
+    private static final int KJOENN = 1;
+    private static final int FDATO = 8;
 
     @SneakyThrows
     public void readSidsFromFile(String filePath, SidCache sidCache) {
@@ -32,9 +34,9 @@ public class SidReader {
     }
 
     public void readSidsFromFile(InputStream inputStream, SidCache sidCache) {
-        FixedWidthFields fieldLengths = new FixedWidthFields(FNR, FNR_NAA, SNR_UTGATT, SNR, DATO_FNR, DATO_FNRNAA , DATO_SNR, RDATO_SNR);
+        FixedWidthFields fieldLengths = new FixedWidthFields(FNR, FNR_NAA, SNR_UTGATT, SNR, DATO_FNR, DATO_FNRNAA, DATO_SNR, RDATO_SNR, KJOENN, FDATO);
         FixedWidthParserSettings settings = new FixedWidthParserSettings(fieldLengths);
-        settings.setHeaders("fnr", "fnr_naa", "snr_utgatt", "snr", "dato_fnr", "dato_fnrnaa", "dato_snr", "rdato_snr");
+        settings.setHeaders("fnr", "fnr_naa", "snr_utgatt", "snr", "dato_fnr", "dato_fnrnaa", "dato_snr", "rdato_snr", "kjoenn", "fdato");
 
         FixedWidthRoutines routines = new FixedWidthRoutines(settings);
         for (SidItem sidItem : routines.iterate(SidItem.class, inputStream, "UTF-8")) {
