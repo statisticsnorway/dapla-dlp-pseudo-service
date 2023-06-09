21385d048fa2ba621bf979c8486148c8aee5d1fc
Anders Lunde <anders-lunde@hotmail.com>
2023-06-09 14:10:06 +0200

    Update to valid test fnr

--- src/main/java/no/ssb/dlp/pseudo/service/sid/SidMappingNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package no.ssb.dlp.pseudo.service.sid;
-
-public class SidMappingNotFoundException extends RuntimeException {
-    public SidMappingNotFoundException(String message) {
-        super(message);
-    }
-}
