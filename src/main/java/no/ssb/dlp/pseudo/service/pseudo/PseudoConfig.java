ccea9b6b62c1f45b274b282b10b056652c53875d
Anders Lunde <anders-lunde@hotmail.com>
2023-06-09 10:18:53 +0200

    Add constructor for a single PseudoFuncRule

--- src/main/java/no/ssb/dlp/pseudo/service/pseudo/PseudoConfig.java
+++ src/main/java/no/ssb/dlp/pseudo/service/pseudo/PseudoConfig.java
@@ -1,6 +1,7 @@
 package no.ssb.dlp.pseudo.service.pseudo;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import no.ssb.dlp.pseudo.core.func.PseudoFuncRule;
 import no.ssb.dlp.pseudo.core.tink.model.EncryptedKeysetWrapper;
 
@@ -8,7 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class PseudoConfig {
     private List<PseudoFuncRule> rules = new ArrayList<>();
     private List<EncryptedKeysetWrapper> keysets = new ArrayList<>();
+
+    public PseudoConfig(PseudoFuncRule pseudoFuncRule){
+        rules.add(pseudoFuncRule);
+    }
 }
