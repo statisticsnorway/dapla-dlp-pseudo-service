7a1994cb5da312717e0c6e24aba9e29d737d1071
Anders Lunde <anders-lunde@hotmail.com>
2023-06-09 11:05:39 +0200

    Add custom RTE for SID mapping not found

---
+++ src/main/java/no/ssb/dlp/pseudo/service/sid/SidMappingNotFoundException.java
@@ -0,0 +1,7 @@
+package no.ssb.dlp.pseudo.service.sid;
+
+public class SidMappingNotFoundException extends RuntimeException {
+    public SidMappingNotFoundException(String message) {
+        super(message);
+    }
+}
