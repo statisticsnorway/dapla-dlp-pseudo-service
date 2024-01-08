895b8ad4b5d6e10b525a36390954d2a90327b85d
Andilun <8294494+Andilun@users.noreply.github.com>
2024-01-08 14:23:54 +0100

    Add tests with null values in process and preprocessor

--- src/test/java/no/ssb/dlp/pseudo/service/pseudo/PseudoFieldTest.java
+++ src/test/java/no/ssb/dlp/pseudo/service/pseudo/PseudoFieldTest.java
@@ -1,18 +1,32 @@
 package no.ssb.dlp.pseudo.service.pseudo;
 
+import io.micronaut.core.async.publisher.Publishers;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.reactivex.Completable;
 import io.reactivex.Flowable;
-import io.reactivex.Single;
-import no.ssb.dlp.pseudo.core.field.FieldDescriptor;
-import no.ssb.dlp.pseudo.core.field.FieldPseudonymizer;
+import io.reactivex.observers.TestObserver;
 import no.ssb.dlp.pseudo.core.map.RecordMapProcessor;
 import no.ssb.dlp.pseudo.core.tink.model.EncryptedKeysetWrapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @MicronautTest
 class PseudoFieldTest {
+
+    @Mock
+    private PseudoConfigSplitter pseudoConfigSplitter;
+
+    @Mock
+    private RecordMapProcessorFactory recordProcessorFactory;
+
+    @Mock
+    private RecordMapProcessor recordMapProcessor;
 
     @Test
     void UsesDefaultPseudoConfigWhenNoKeysetIsSupplied() {
@@ -38,4 +52,56 @@ class PseudoFieldTest {
         assertEquals(encryptedKeysetWrapper,
                 pseudoField.getPseudoConfig().getKeysets().get(0));
     }
+
+    void setUpProcessorMocks() {
+        MockitoAnnotations.openMocks(this);
+        when(pseudoConfigSplitter.splitIfNecessary(any())).thenReturn(Collections.singletonList(new PseudoConfig()));
+        when(recordProcessorFactory.newPseudonymizeRecordProcessor(any())).thenReturn(recordMapProcessor);
+    }
+
+    @Test
+    void processWithNullValues() {
+        setUpProcessorMocks();
+
+        //Preprocessor logic is covered in #preprocessorWithNullValues
+        when(recordMapProcessor.hasPreprocessors()).thenReturn(false);
+
+        when(recordMapProcessor.process(any())).thenAnswer(invocation -> {
+            Map<String, String> argument = invocation.getArgument(0);
+            String originalValue = argument.get("testField");
+            return Collections.singletonMap("testField", "processedValue " + originalValue);
+        });
+
+        PseudoField pseudoField = new PseudoField("testField", null, null);
+        List<String> values = Arrays.asList("v1", null, "v2");
+
+        Flowable<List<Object>> result = pseudoField.process(pseudoConfigSplitter, recordProcessorFactory, values);
+        List<List<Object>> resultList = result.toList().blockingGet();
+        assertEquals(List.of(List.of("processedValue v1", Optional.empty(), "processedValue v2")), resultList);
+        assertEquals(3, resultList.get(0).size());
+
+        // Verify that recordMapProcessor was called once for each non-null value
+        verify(recordMapProcessor, times(2)).process(any());
+    }
+
+    @Test
+    void preprocessorWithNullValues() {
+        setUpProcessorMocks();
+
+        when(recordMapProcessor.hasPreprocessors()).thenReturn(true);
+        when(recordMapProcessor.init(any())).thenReturn(Collections.singletonMap("testField", "initializedValue"));
+
+        PseudoField pseudoField = new PseudoField("testField", null, null);
+        List<String> values = Arrays.asList("v1", null, "v2");
+
+        Completable result = pseudoField.getPreprocessor(values, recordMapProcessor);
+
+        TestObserver<Void> testObserver = result.test();
+        testObserver.assertComplete();
+        testObserver.assertNoErrors();
+
+        // Verify that recordMapProcessor was called once for each non-null value
+        verify(recordMapProcessor, times(2)).init(any());
+    }
+
 }
