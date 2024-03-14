fabb023a763ac5647f37c80eb464ea17e7ecb50a
rpk <rupinder.kaur@ssb.no>
2024-03-14 10:38:31 +0100

    Fixed check for null object returned from lookup service. Added changes to support new map-sid

--- src/main/java/no/ssb/dlp/pseudo/service/pseudo/RecordMapProcessorFactory.java
+++ src/main/java/no/ssb/dlp/pseudo/service/pseudo/RecordMapProcessorFactory.java
@@ -4,6 +4,7 @@ import jakarta.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import no.ssb.dapla.dlp.pseudo.func.PseudoFuncInput;
 import no.ssb.dapla.dlp.pseudo.func.PseudoFuncOutput;
+import no.ssb.dapla.dlp.pseudo.func.TransformDirection;
 import no.ssb.dapla.dlp.pseudo.func.fpe.FpeFunc;
 import no.ssb.dapla.dlp.pseudo.func.map.MapFunc;
 import no.ssb.dapla.dlp.pseudo.func.map.MapFuncConfig;
@@ -44,7 +45,7 @@ public class RecordMapProcessorFactory {
         for (PseudoConfig config : pseudoConfigs) {
             final PseudoFuncs fieldPseudonymizer = newPseudoFuncs(config.getRules(),
                     pseudoKeysetsOf(config.getKeysets()));
-            chain.preprocessor((f, v) -> init(fieldPseudonymizer, f, v));
+            chain.preprocessor((f, v) -> init(fieldPseudonymizer, f, v, TransformDirection.APPLY));
             chain.register((f, v) -> process(PSEUDONYMIZE, fieldPseudonymizer, f, v, metadataProcessor));
         }
         return new RecordMapProcessor<>(chain, metadataProcessor);
@@ -57,7 +58,7 @@ public class RecordMapProcessorFactory {
         for (PseudoConfig config : pseudoConfigs) {
             final PseudoFuncs fieldDepseudonymizer = newPseudoFuncs(config.getRules(),
                     pseudoKeysetsOf(config.getKeysets()));
-            chain.preprocessor((f, v) -> init(fieldDepseudonymizer, f, v));
+            chain.preprocessor((f, v) -> init(fieldDepseudonymizer, f, v, TransformDirection.RESTORE));
             chain.register((f, v) -> process(DEPSEUDONYMIZE, fieldDepseudonymizer, f, v, metadataProcessor));
         }
 
@@ -83,10 +84,10 @@ public class RecordMapProcessorFactory {
         return new PseudoFuncs(rules, pseudoSecrets.resolve(), keysets);
     }
 
-    private String init(PseudoFuncs pseudoFuncs, FieldDescriptor field, String varValue) {
+    private String init(PseudoFuncs pseudoFuncs, FieldDescriptor field, String varValue, TransformDirection direction) {
         if (varValue != null) {
             pseudoFuncs.findPseudoFunc(field).ifPresent(pseudoFunc ->
-                    pseudoFunc.getFunc().init(PseudoFuncInput.of(varValue)));
+                    pseudoFunc.getFunc().init(PseudoFuncInput.of(varValue), direction));
         }
         return varValue;
     }
